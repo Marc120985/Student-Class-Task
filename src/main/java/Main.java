6e9f4d6ef3bbb57ce5524c79dc767db2d4dd992d
Marc120985 <creativ.zeit@gmail.com>
2022-10-04 11:03:44 +0300

    add var to class Student

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Student student = new Student("Asem", 1234567);
+        //System.out.println(student.name);
     }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,4 +1,12 @@
+import lombok.Data;
+
+@Data
 public class Student {
     String name;
     int id;
+
+    public Student(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
 }
