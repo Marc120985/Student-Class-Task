36cead7735122a3c670af8a774a61185179dc78d
Marc120985 <creativ.zeit@gmail.com>
2022-10-04 11:22:52 +0300

    add method getAllStudents

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student("Asem", 1234567);
         //System.out.println(student.name);
+        Student student2 = new Student("Christian", 876543321);
+
+        Student[] students = {student, student2};
+        //System.out.println(students[1])
+        StudentDB studentData = new StudentDB(students);
+        //System.out.println(studentData);
+        System.out.println(studentData.getAllStudents());
+
     }
 }
--- src/main/java/StudentDB.java
+++ src/main/java/StudentDB.java
@@ -1,2 +1,13 @@
+import lombok.Data;
+
+@Data
 public class StudentDB {
+    Student[] students;
+
+    public StudentDB(Student[] students) {
+        this.students = students;
+    }
+    public Student[] getAllStudents(){
+        return students;
+    }
 }
